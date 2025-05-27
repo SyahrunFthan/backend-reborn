41a8a55a4fbb2609fa6ce627b44b5f9a5b488997
syarifsakur <107300773+syarifsakur@users.noreply.github.com>
2025-05-28 01:59:15 +0800

    feat: controllers,router,schema dari Region

--- src/controllers/Region.js
+++ src/controllers/Region.js
@@ -1,6 +1,54 @@
 import Region from '../models/ModelRegion.js';
 import VillageApparatus from '../models/ModelAparatus.js';
 
+// all role
+export const getRegion = async (req, res) => {
+  try {
+    const response = await Region.findAll({
+      attributes: [
+        'uuid',
+        'name',
+        'leader_id',
+        'total_population',
+        'hectare_area',
+        'geo_polygon',
+        'centroid_lat',
+        'centroid_long',
+        'map_color',
+      ],
+    });
+
+    return res.status(200).json({ response });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// all role
+export const getRegionById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await Region.findByPk(id, {
+      attributes: [
+        'uuid',
+        'name',
+        'leader_id',
+        'total_population',
+        'hectare_area',
+        'geo_polygon',
+        'centroid_lat',
+        'centroid_long',
+        'map_color',
+      ],
+    });
+
+    return res.status(200).json({ response });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// Admin
 export const createRegion = async (req, res) => {
   const {
     name: nameRegion,
@@ -30,5 +78,58 @@ export const createRegion = async (req, res) => {
     return res.status(201).json({ message: 'Berhasil Menambah Region' });
   } catch (error) {
     return res.status(500).json({ error });
+  }
+};
+
+// Admin
+export const updateRegion = async (req, res) => {
+  const {
+    name: nameRegion,
+    total_population,
+    hectare_area,
+    geo_polygon,
+    centroid_lat,
+    centroid_long,
+    map_color,
+    leader_id,
+  } = req.body;
+  const { name } = req;
+  const { id } = req.params;
+
+  const region = await Region.findByPk(id);
+
+  try {
+    await region.update({
+      name: nameRegion,
+      leader_id,
+      total_population,
+      hectare_area,
+      geo_polygon,
+      centroid_lat,
+      centroid_long,
+      map_color,
+      updated_by: name,
+    });
+
+    return res
+      .status(200)
+      .json({ message: 'Berhasil mengupdate data Region!' });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// Admin
+export const deleteRegion = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const region = await Region.findByPk(id);
+
+    await region.destroy();
+
+    return res.status(200).json({ message: 'Berhasil Menghapus Region!' });
+  } catch (error) {
+    return res.status(500).json(500);
   }
 };
